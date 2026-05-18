3be81133dafe84a8c5e2baf5bfe1998bdb69363b
ameinhardt <26243798+ameinhardt@users.noreply.github.com>
2026-05-18 21:52:47 +0200

    chore: highlight issue
    Signed-off-by: ameinhardt <26243798+ameinhardt@users.noreply.github.com>

--- src/example.test.ts
+++ src/example.test.ts
@@ -20,7 +20,9 @@ Object.entries({
     test(`${name}: can be deleted`, async () => {
       const em = orm.em.fork();
       // Removing the 'heavyUsers' pointer from Device (and counterpart from HeavyUser) lets this test succeed
+      // this has 'devices' as expected
       assert(em.getMetadata(NormalUser).bidirectionalRelations.find((prop) => prop.name === 'devices') != null, "NormalUser has a 'devices' bidirectionalRelations");
+      // ⚡ unexpectedly, this metadata has 'devices', too:
       assert(em.getMetadata(CasualUser).bidirectionalRelations.find((prop) => prop.name === 'devices' as EntityKey<CasualUser>) == null, "CasualUser does not have 'devices' bidirectionalRelations");
       
       await em.flush();
