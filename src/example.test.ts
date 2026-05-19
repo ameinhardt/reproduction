5ecc57d9012a77168cb101a04bae2bd85fe114d9
ameinhardt <26243798+ameinhardt@users.noreply.github.com>
2026-05-19 16:02:33 +0200

    feat: showcase impact of metadata confusion
    Signed-off-by: ameinhardt <26243798+ameinhardt@users.noreply.github.com>

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,11 +1,12 @@
 import { EntityKey, MikroORM, Options } from '@mikro-orm/core';
 import { CasualUser, NormalUser } from './user.entity.ts';
-import { sqlConfig } from './mikro-orm.config.ts'
+import { sqlConfig, mongoConfig } from './mikro-orm.config.ts'
 
 let orm: MikroORM;
 
 Object.entries({
-  sql: sqlConfig
+  // sql: sqlConfig,
+  mongo: mongoConfig
 }).forEach(([name, config]) => {
   describe(name, () => {
     beforeAll(async () => {
@@ -17,15 +18,35 @@ Object.entries({
       await orm.close();
     });
 
-    test(`${name}: can be deleted`, async () => {
+     test(`${name}: can be deleted when in one em`, async () => {
       const em = orm.em.fork();
-      // Removing the 'heavyUsers' pointer from Device (and counterpart from HeavyUser) lets this test succeed
-      // this has 'devices' as expected
-      assert(em.getMetadata(NormalUser).bidirectionalRelations.find((prop) => prop.name === 'devices') != null, "NormalUser has a 'devices' bidirectionalRelations");
-      // ⚡ unexpectedly, this metadata has 'devices', too:
-      assert(em.getMetadata(CasualUser).bidirectionalRelations.find((prop) => prop.name === 'devices' as EntityKey<CasualUser>) == null, "CasualUser does not have 'devices' bidirectionalRelations");
       
+      const { id } = em.create(CasualUser, {
+        name: 'Casual User',
+        type: 'casual'
+      });
+      em.flush();
+      
+      const user = await em.findOne(CasualUser, { id });
+      assert(user != null);
+      em.remove(user);
       await em.flush();
+    });
+
+    test(`${name}: can be deleted when in two em`, async () => {
+      const em1 = orm.em.fork();
+      
+      const { id } = em1.create(CasualUser, {
+        name: 'Casual User',
+        type: 'casual'
+      });
+      em1.flush();
+      
+      const em2 = orm.em.fork();
+      const user = await em2.findOne(CasualUser, { id });
+      assert(user != null);
+      em2.remove(user);
+      await em2.flush();
     });
   });
 });
